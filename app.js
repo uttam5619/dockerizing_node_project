dc57558a5573265ae62dd910219b34b92e5681da
uttam kumar <uttamkumar050519@gmail.com>
2026-03-13 23:31:26 +0530

    added some dummy data in response

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ app.use(express.json())
 
 const responseObject={
     success:true,
-    message:'application working'
+    message:'application working',
+    dummyData:[
+        {
+            name:'mohan',
+            email:'mohan@gmail.com'
+        },
+        {
+            name:'sohan',
+            email:'sohan@gmail.com'
+        }
+    ]
 }
 
 app.get('/',(req,res)=>{
